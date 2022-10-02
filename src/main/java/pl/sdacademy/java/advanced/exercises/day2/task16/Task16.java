30fda57fd5466e3f302da3eeda8101dbd4d22415
Pawel Dudko <paweldudko@outlook.com>
2022-10-02 12:36:33 +0200

    solution for day2.task16

---
+++ src/main/java/pl/sdacademy/java/advanced/exercises/day2/task16/Task16.java
@@ -0,0 +1,8 @@
+package pl.sdacademy.java.advanced.exercises.day2.task16;
+
+public class Task16 {
+    public static void main(String[] args) {
+        Runner result = Runner.getFitnessLevel(512);
+        System.out.println(result);
+    }
+}
