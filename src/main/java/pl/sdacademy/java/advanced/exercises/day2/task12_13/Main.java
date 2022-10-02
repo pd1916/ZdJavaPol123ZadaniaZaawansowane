f15c41dc9abf1febc7eea384057d1b0fe32ad51e
Pawel Dudko <paweldudko@outlook.com>
2022-10-02 10:32:45 +0200

    added initial tests for day2.task12_task13

--- src/main/java/pl/sdacademy/java/advanced/exercises/day2/task12_13/Main.java
+++ src/main/java/pl/sdacademy/java/advanced/exercises/day2/task12_13/Main.java
@@ -4,8 +4,8 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Car car1 = new Car("aaa", "bbb", 1000, 2000,null, EngineType.V8);
-        Car car2 = new Car("ccc", "ddd", 1000, 2000,null, EngineType.V12);
+        Car car1 = new Car("aaa", "bbb", 1000, 2000, null, EngineType.V8);
+        Car car2 = new Car("ccc", "ddd", 1000, 2000, null, EngineType.V12);
 
         CarService carService = new CarService();
         carService.add(car1);
