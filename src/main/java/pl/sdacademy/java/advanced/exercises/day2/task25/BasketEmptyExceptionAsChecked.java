056b17c5e8c50889a148c3d49d0c2fd410219bcb
Pawel Dudko <paweldudko@outlook.com>
2022-10-02 15:47:10 +0200

    solution for day2.task25

---
+++ src/main/java/pl/sdacademy/java/advanced/exercises/day2/task25/BasketEmptyExceptionAsChecked.java
@@ -0,0 +1,7 @@
+package pl.sdacademy.java.advanced.exercises.day2.task25;
+
+public class BasketEmptyExceptionAsChecked extends Exception {
+    public BasketEmptyExceptionAsChecked(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/pl/sdacademy/java/advanced/exercises/day2/task25/BasketFullExceptionAsChecked.java
@@ -0,0 +1,7 @@
+package pl.sdacademy.java.advanced.exercises.day2.task25;
+
+public class BasketFullExceptionAsChecked extends Exception {
+    public BasketFullExceptionAsChecked(String message) {
+        super(message);
+    }
+}
